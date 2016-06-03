f51581a4808a7506103f5b2abe5b8cae3463b9ac
FractalXX <fade@hotmail.hu>
2016-06-03 12:49:50 +0200

    nothing

--- first.js
+++ first.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
 function handleRequest(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end("hello world\n");
 }
 
-http.createServer(handleRequest).listen(0);
+http.createServer(handleRequest).listen(port, ipaddress);
