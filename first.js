5e89876c42ef4a8fc191b31d0b82510c98da7d5f
FractalXX <fade@hotmail.hu>
2016-06-03 12:25:53 +0200

    nothing

--- first.js
+++ first.js
@@ -1,12 +1,11 @@
 const http = require('http');
 
+const express = require('express');
+const app = express();
+
 function handleRequest(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end("hello world\n");
 }
 
 http.createServer(handleRequest).listen(8000);
-
-app.get('/', function (req, res) {
-    res.status('200').send('Service is up');
-});
