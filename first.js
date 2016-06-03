864ed533a48bbcedd9be57f6916e8b619860327f
FractalXX <fade@hotmail.hu>
2016-06-03 09:15:36 +0200

    added first

---
+++ first.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+function handleRequest(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end("hello world\n");
+}
+
+http.createServer(handleRequest).listen(8000);
